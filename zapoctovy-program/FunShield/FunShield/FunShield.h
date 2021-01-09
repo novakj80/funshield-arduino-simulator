c99fe609cdc2e98e6bc77138b610c0182a40d07e
Jaromír Novák <88921589@o365.cuni.cz>
2021-01-09 06:41:11 +0100

    Funshield interface

--- zapoctovy-program/FunShield/FunShield/FunShield.h
+++ zapoctovy-program/FunShield/FunShield/FunShield.h
@@ -2,3 +2,31 @@
 
 #include <iostream>
 
+enum pin_level {LOW, HIGH};
+enum pin_mode {INPUT, OUTPUT};
+
+void pinMode();
+
+void digitalWrite();
+pin_level digiralRead(int pin);
+void shiftOut();
+
+void delay(unsigned long ms);
+void delayMicroseconds(unsigned int us);
+unsigned long millis();
+unsigned long micros();
+
+// Strings
+#define isAlpha isalpha
+#define isAlphaNumeric isalnum
+// isAscii()
+#define isControl iscntrl
+#define isDigit isdigit
+#define isGraph isgraph
+#define isHexadecimalDigit isxdigit
+#define isLowerCase islower
+#define isPrintable isprint
+#define isPunct ispunct
+#define isSpace isspace
+#define isUpperCase isupper
+#define isWhitespace isblank
