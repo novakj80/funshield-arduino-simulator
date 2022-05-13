d4242ab23e78be32597abd6ef15b68b1698785ec
Jaromír Novák <88921589@o365.cuni.cz>
2022-05-13 10:17:47 +0200

    Change hello world program Interactive program - LED is on when button is pressed

--- zapoctovy-program/funshield/src/hello_world.ino
+++ zapoctovy-program/funshield/src/hello_world.ino
@@ -2,10 +2,11 @@
 using namespace fs;
 void setup() {
     // Run once
-    pinMode(13, OUTPUT); // Nastavíme režim pinu, kde je dioda pøipojená, na výstupní
+    pinMode(13, OUTPUT); // Set LED pin as input pin
+    pinMode(15, INPUT);  // Set button pin as output pin
 }
 
 void loop() {
     // Run repeatedly
-	digitalWrite(13, digitalRead(13) ? LOW : HIGH); // Zapsáním LOW na pin, kde je dioda pøipojená, LED rozsvítíme
+	digitalWrite(13, digitalRead(A1)); // Light LED while button is pressed
 }
